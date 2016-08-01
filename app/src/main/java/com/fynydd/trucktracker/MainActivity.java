3885a08d810d7b817febb815b6d688ed0095cb88
Timothy Tentinger <ttentinger@gmail.com>
2016-08-01 17:19:46 -0400

    First initial upload to repository

--- app/src/main/java/com/fynydd/trucktracker/MainActivity.java
+++ app/src/main/java/com/fynydd/trucktracker/MainActivity.java
@@ -14,7 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+// import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
